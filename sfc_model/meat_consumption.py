a8eb4f8a5ba9269b251717da1e6c508ea380fa43
Husnul Abid <habid96@gmail.com>
2023-09-21 01:20:24 +0200

    api buiding

--- sfc_model/meat_consumption.py
+++ sfc_model/meat_consumption.py
@@ -6,6 +6,6 @@ meat_consumption_page = Blueprint('meat_consumption_page', __name__, template_fo
 
 @meat_consumption_page.route('/analysis/meat_consumption_pie', methods=['GET'])
 def meat_consumption_pie():
-    result = { 'meat_consumption': '40%'}
+    result = { 'popularity': '70%', 'avg consumption': '1.3 kg'}
 
     return jsonify(result)
