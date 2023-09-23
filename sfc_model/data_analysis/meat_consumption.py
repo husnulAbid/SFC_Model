7030c1c4653bee80932b84b0167c85581b625e98
Husnul Abid <habid96@gmail.com>
2023-09-23 13:23:32 +0200

    meat consumption pie helper function

--- sfc_model/data_analysis/meat_consumption.py
+++ sfc_model/data_analysis/meat_consumption.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 
+Current_year = 2023
+dataset_file_name = 'meat_consumption_worldwide.csv'
+
 def read_dataset(dataset_file_name):
     
     raw_df = pd.read_csv(f'datasets\{dataset_file_name}')
+    raw_df = raw_df.loc[raw_df['TIME'] <= Current_year] 
 
     return raw_df
 
 
-def get_total_consumption_over_time(df, country_name, time_start, time_end):
-
+def get_all_type_consumption_over_time(df, country_name, time_start, time_end):
     df = df.loc[df['LOCATION'] == country_name]
-
     dfThTon = df.loc[df['MEASURE'] == 'THND_TONNE']
     dfThTon.drop(['LOCATION', 'MEASURE'], axis=1, inplace=True)
 
@@ -25,16 +27,30 @@ def get_total_consumption_over_time(df, country_name, time_start, time_end):
 
 
 
-def get_country_stat(country_name, time_start, time_end):
-
-    dataset_file_name = 'meat_consumption_worldwide.csv'
+def get_country_stat_on_all_type_consumption(country_name, time_start, time_end):
     df = read_dataset(dataset_file_name)
-
-    consumption_over_time = get_total_consumption_over_time(df, country_name, time_start, time_end)
+    consumption_over_time = get_all_type_consumption_over_time(df, country_name, time_start, time_end)
 
     return consumption_over_time
 
 
+def get_countries():
+    df = read_dataset(dataset_file_name)
+    available_countries = df['LOCATION'].unique().tolist()
+    countries_dict = {'countries': available_countries}
+
+    return countries_dict
+
+
+def get_available_years():
+    df = read_dataset(dataset_file_name)
+    
+    start_year = df['TIME'].min()
+    end_year = df['TIME'].max()
+
+    years_dict = {'start_year': str(start_year), 'end_year': str(end_year)}
+
+    return years_dict
 
 
 # print('\n\n-------------------------\n\n')
--- sfc_model/data_analysis_page.py
+++ sfc_model/data_analysis_page.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, jsonify, request
 
-from data_analysis.meat_consumption import get_country_stat
+from data_analysis.meat_consumption import get_country_stat_on_all_type_consumption, get_countries, get_available_years
 
 
 data_analysis_page = Blueprint('data_analysis_page', __name__, template_folder='templates')
 
 
-# sample : http://127.0.0.1:5000/analysis/meat_consumption_time?country=NZL&start=1991&end=1995
+# sample : {base_url}/analysis/meat_consumption_time?country=NZL&start=1991&end=1995
 
 @data_analysis_page.route('/analysis/meat_consumption_time', methods=['GET'])
 def meat_consumption_pie():
@@ -16,6 +16,24 @@ def meat_consumption_pie():
     time_start = int(args.get('start'))
     time_end = int(args.get('end'))
 
-    result = get_country_stat(country_name, time_start, time_end)
+    result = get_country_stat_on_all_type_consumption(country_name, time_start, time_end)
 
     return jsonify(result)
+
+
+# sample : {base_url}/analysis/meat_consumption_time/countries
+
+@data_analysis_page.route('/analysis/meat_consumption_time/countries', methods=['GET'])
+def meat_consumption_available_countries():
+    available_countries = get_countries()
+
+    return jsonify(available_countries)
+
+
+# sample : {base_url}/analysis/meat_consumption_time/start_and_end_year
+
+@data_analysis_page.route('/analysis/meat_consumption_time/start_and_end_year', methods=['GET'])
+def meat_consumption_pie_start_and_end_year():
+    available_years = get_available_years()
+
+    return jsonify(available_years)
